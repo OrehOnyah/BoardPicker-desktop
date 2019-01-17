75cd9971a854f74c4e37ae8b5cc6063a60002485
Jung Jinwook <jinwook1240@gmail.com>
2019-01-18 02:17:44 +0900

    html, css, javascript(UI)
    UI changing
    make require() on html to `<script>`tag external js file,
    make .pixel class on css to expresson of low quality picture seems to be pixeled
    Issue https://github.com/OrehOnyah/BoardPicker-desktop/issues/1

--- ui_script/index.js
+++ ui_script/index.js
@@ -1,3 +1,6 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+window.onload = function(){
+    document.getElementById('header_divider').style.marginTop = document.getElementsByTagName('header')[0].getBoundingClientRect().height + "px";
+};
