4f58913bc701c274168fd5a9cf697ab4d175b017
Jung Jinwook <jinwook1240@gmail.com>
2019-01-21 03:13:28 +0900

    웹뷰 내부 태그에 대한 onclick 붙이기 코드 작성중
    현재 돌아가지 않는 이유
    -> index_webview_inject.js에서 onload로 해당 작업을 수행하도록 만듬.
    따라서 attachEvent 및 addEventListener를 빼고 웹뷰가 로딩되었다면 onclick을 붙이게 하는 코드 추가가 필요함.
    Issue https://github.com/OrehOnyah/BoardPicker-desktop/issues/11

--- ui_script/index.js
+++ ui_script/index.js
@@ -2,34 +2,60 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 const UI_DEBUGGING = false;
-const WEBVIEW_TRANSPORT_DEBUGGING=true;
-window.onload = function(){/*
+const WEBVIEW_TRANSPORT_DEBUGGING = false;
+window.onload = function () {/*
     document.getElementById('header_divider').style.marginTop = document.getElementsByTagName('header')[0].getBoundingClientRect().height + "px";*/
     //메인 webview 높이 맞춤
-    document.getElementById('webview_main').style.height=(window.innerHeight-document.getElementsByTagName('header')[0].getBoundingClientRect().height)+'px';
+    document.getElementById('webview_main').style.height = (window.innerHeight - document.getElementsByTagName('header')[0].getBoundingClientRect().height) + 'px';
+    //페이지 클래스를 가진 섹션을 윈도우 크기로 맞춤
     makeSectionsToPage();
 };
-window.onresize = function(){
+window.onresize = function () {
     //메인 webview 높이 맞춤
-    document.getElementById('webview_main').style.height=(window.innerHeight-document.getElementsByTagName('header')[0].getBoundingClientRect().height)+'px';
-    if(WEBVIEW_TRANSPORT_DEBUGGING){
-        document.getElementById('webview_main').executeJavaScript('__BoardPickerTools.test_trans_func()' ,function(asdf){
+    document.getElementById('webview_main').style.height = (window.innerHeight - document.getElementsByTagName('header')[0].getBoundingClientRect().height) + 'px';
+    //웹뷰 내부 통신 테스트코드.
+    if (WEBVIEW_TRANSPORT_DEBUGGING) {
+        document.getElementById('webview_main').executeJavaScript('__BoardPickerTools.test_trans_func()', function (asdf) {
             alert(asdf);
         });
     }
+    //페이지 클래스를 가진 섹션을 윈도우 크기로 맞춤
     makeSectionsToPage();
 };
-function makeSectionsToPage(){//모든 페이지 클래스를 가지는 태그에 대해 하나의 페이지 크기로 만들어버림.
+
+function makeSectionsToPage() {//모든 페이지 클래스를 가지는 태그에 대해 하나의 페이지 크기로 만들어버림.
     let elements = document.getElementsByClassName('page');
-    for(let i in elements){
+    for (let i in elements) {
         makeElementWithPage(elements[i]);
     }
 }
-function makeElementWithPage(element){
-    element.style.height=window.innerHeight-document.getElementsByTagName('header')[0].getBoundingClientRect().height+'px';
-    element.style.marginTop=document.getElementsByTagName('header')[0].getBoundingClientRect().height+'px';
-    element.style.marginBottom='0px';
-    if(UI_DEBUGGING){
-        element.style.border="1px solid red";
+
+function makeElementWithPage(element) {
+    element.style.height = window.innerHeight - document.getElementsByTagName('header')[0].getBoundingClientRect().height + 'px';
+    element.style.marginTop = document.getElementsByTagName('header')[0].getBoundingClientRect().height + 'px';
+    element.style.marginBottom = '0px';
+    if (UI_DEBUGGING) {
+        element.style.border = "1px solid red";
     }
+}
+
+/* 버튼 이벤트 관련 */
+if(window.addEventListener){
+    window.addEventListener('load', function () {
+        alert('index.js');
+        document.getElementById('add_icon').onclick=function(){
+            document.getElementById('webview_main').executeJavaScript('__BoardPickerTools.set_atag_onclick()', function (asdf) {
+                alert('inject succesed');
+            });
+        }
+    });
+}
+else if(window.attachEvent){
+    window.attachEvent('onload', function () {
+        document.getElementById('add_icon').onclick=function(){
+            document.getElementById('webview_main').executeJavaScript('__BoardPickerTools.set_atag_onclick()', function (asdf) {
+                alert('inject succesed');
+            });
+        }
+    });
 }
--- ui_script/index_webview_inject.js
+++ ui_script/index_webview_inject.js
@@ -1,8 +1,33 @@
-__BoardPickerTools={
-    window:window,
-    document:document,
-    test_trans_func:function(){
+__BoardPickerTools = {
+    test_trans_func: function () {
         alert('test_called');
         return "testString";
+    },
+    set_atag_onclick() {
+        alert('atag_onclick called');
+        if(window.addEventListener){
+            window.addEventListener('load',function(){
+                let elements = document.getElementsByTagName('*');
+                alert(elements+' ');
+                for (let i in elements) {
+                    elements[i].onclick = function (event) {
+                        alert('element clicked');
+                        event.stopPropagation();
+                    }
+                }
+            });
+        }
+        else if(window.attachEvent){
+            window.attachEvent('load',function(){
+                let elements = document.getElementsByTagName('*');
+                alert(elements);
+                for (let i in elements) {
+                    elements[i].onclick = function (event) {
+                        alert('element clicked');
+                        event.stopPropagation();
+                    }
+                }
+            });
+        }
     }
 };
